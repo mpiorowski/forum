c00a46ffde88ace5347bd89f3c77784621aa8974
mpiorowski <mateuszpiorowski@gmail.com>
2021-02-04 00:58:55 +0100

    working auth

--- src/pages/api/categories/index.tsx
+++ src/pages/api/categories/index.tsx
@@ -1,28 +1,34 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/client";
 import { Pool, QueryResult } from "pg";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const pool = new Pool();
-  const client = await pool.connect();
-  let response: QueryResult;
-  try {
-    await client.query("BEGIN");
-    if (req.method === "GET") {
-      const selectAllCategories = `select * from forum_categories`;
-      response = await client.query(selectAllCategories);
-    } else if (req.method === "POST") {
-      const category = JSON.parse(req.body);
-      console.log(category);
-      
-      const addCategory = `insert into forum_categories (title, description, "fkUserId", icon) values('${category.title}', '${category.description}', 1, 'test')`;
-      response = await client.query(addCategory);
+  const session = await getSession({ req });
+  if (session) {
+    const pool = new Pool();
+    const client = await pool.connect();
+    let response: QueryResult;
+    try {
+      await client.query("BEGIN");
+      if (req.method === "GET") {
+        response = await client.query(`select * from forum_categories`);
+      } else if (req.method === "POST") {
+        const category = JSON.parse(req.body);
+        const userId = await client.query(`select id from users where email = '${session.user.email}'`);
+        const addCategory = `insert into forum_categories (title, description, userid, icon) values('${category.title}', '${category.description}', ${userId.rows[0].id}, 'test')`;
+        response = await client.query(addCategory);
+      }
+      await client.query("COMMIT");
+    } catch (e) {
+      await client.query("ROLLBACK");
+      throw e;
+    } finally {
+      client.release();
     }
-    await client.query("COMMIT");
-  } catch (e) {
-    await client.query("ROLLBACK");
-    throw e;
-  } finally {
-    client.release();
+    return res.status(200).send(response.rows);
+  } else {
+    // Not Signed in
+    res.status(401);
   }
-  return res.status(200).send(response.rows);
+  res.end();
 }
